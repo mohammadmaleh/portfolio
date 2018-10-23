0a79c3d8dba88b54a957e28b0868a153a40a4ffa
Mohammad maleh <mohammad.maleh@gmail.com>
2018-10-23 18:13:05 +0300

    first push

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-app.get('/', function (req, res) {
-res.send(JSON.stringify({ Hello: 'World'}));
-});
-app.listen(port, function () {
-    console.log('Example app listening on port !');
-});
+import express from "express";
+import path from "path";
+
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Layout from "./components/Layout";
+
+const app = express();
+
+app.use( express.static( path.resolve( __dirname, "../dist" ) ) );
+
+app.get( "/*", ( req, res ) => {
+    const jsx = ( <Layout /> );
+    const reactDom = renderToString( jsx );
+
+    res.writeHead( 200, { "Content-Type": "text/html" } );
+    res.end( htmlTemplate( reactDom ) );
+} );
+
+app.listen( 2048 );
+
+function htmlTemplate( reactDom ) {
+    return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>React SSR</title>
+        </head>
+        
+        <body>
+            <div id="app">${ reactDom }</div>
+            <script src="./app.bundle.js"></script>
+        </body>
+        </html>
+    `;
+}
